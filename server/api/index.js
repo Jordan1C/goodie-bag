62d98a832e0fb1977c3982aedd43946746e6efa4
Jordan Contract <jordan59@optonline.net>
2019-10-12 18:07:35 -0400

    seeded table with completed model def and added all routes including those for extra credit

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,7 @@
-'use strict'
+"use strict";
+import Candy from "../server/db/models/Candy";
 
-const router = require('express').Router()
+const router = require("express").Router();
 
 // Your routes go here!
 // NOTE: Any routes that you put here are ALREADY mounted on `/api`
@@ -18,10 +19,57 @@ const router = require('express').Router()
 // but you DON'T have a corresponding router, this piece of
 // middleware will generate a 404, and send it to your
 // error-handling endware!
-router.use((req, res, next) => {
-  const err = new Error('API route not found!')
-  err.status = 404
-  next(err)
-})
 
-module.exports = router
+router.get("/candies", async (req, res, next) => {
+  try {
+    const allCandies = await Candy.findAll();
+    if (!allCandies) res.sendStatus(404);
+    else res.json(allCandies);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/candies/:id", async (req, res, next) => {
+  try {
+    const candy = await Candy.findById(req.params.id);
+    if (!candy) res.sendStatus(404);
+    else res.json(candy);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/candies", async (req, res, next) => {
+  try {
+    Candy.create(req.body).then(candy => res.status(201).send(candy));
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/candies/:id", async (req, res, next) => {
+  try {
+    const candy = await Candy.findById(req.params.id);
+    candy.update(req.body).then(candy => res.status(200).send(candy));
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/candies/:id", async (req, res, next) => {
+  try {
+    const candy = await Candy.findById(req.params.id);
+    candy.destroy(req.body).then(candy => res.status(204).send(candy));
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.use((req, res, next) => {
+  const err = new Error("API route not found!");
+  err.status = 404;
+  next(err);
+});
+
+module.exports = router;
--- server/db/models/Candy.js
+++ server/db/models/Candy.js
@@ -1,6 +1,35 @@
-const Sequelize = require('sequelize');
-const db = require('../database');
+const Sequelize = require("sequelize");
+const db = require("../database");
 
-module.exports = db.define('candy', {
+module.exports = db.define("candy", {
   // define your model here!
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0,
+    validate: {
+      min: 0,
+      max: 10
+    }
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: "https://i.ytimg.com/vi/Csq2pilYDco/hqdefault.jpg",
+    validate: {
+      isUrl: true
+    }
+  }
 });
